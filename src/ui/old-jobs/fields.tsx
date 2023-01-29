b15361571d22166a8dc855a0b9b0452a20a4f823
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-01-28 23:30:06 -0500

    deleted old files

--- src/ui/old-jobs/fields.tsx
+++
@@ -1,9 +0,0 @@
-import Status from './fields/status';
-import Applied from './fields/applied';
-import EditableText from './fields/text';
-
-export const JobField = {
-    ...Status,
-    ...Applied,
-    ...EditableText,
-}
