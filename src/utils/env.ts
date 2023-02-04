7087ad147d72ad2fc290ae71ad66435137aabf11
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-02-04 18:05:03 -0500

    adjusted env code

--- src/utils/env.ts
+++ src/utils/env.ts
@@ -1,6 +1,12 @@
 import { cleanEnv, str } from 'envalid';
 import * as dotenv from 'dotenv';
+
 dotenv.config();
+dotenv.config({
+    override: true,
+    path: process.cwd() + '/.env.local',
+});
+
 
 export const env = cleanEnv(process.env, {
     DATASET: str({
