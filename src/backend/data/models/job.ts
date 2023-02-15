23f46b9509379f31a2d927ed27a910101f8cab27
Chris Seitz <54898870+cseitz@users.noreply.github.com>
2023-02-15 13:29:42 -0500

    fixed default date

--- src/backend/data/models/job.ts
+++ src/backend/data/models/job.ts
@@ -9,7 +9,7 @@ export type JobData = z.infer<typeof jobData>;
 export const jobData = modelData.extend({
     title: z.string(),
     company: z.string(),
-    applied: z.date().default(new Date()),
+    applied: z.date().default(() => new Date()),
     status: z.enum(['pending', 'rejected', 'interview', 'offered', 'accepted', 'declined']).default('pending'),
     offer: z.string().optional(),
     link: z.string().optional(),
